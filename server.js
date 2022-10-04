1feca97522f7076168f97a9cb86fdf0a51e01d4a
Becky Brown <bardbarienne@gmail.com>
2022-10-03 23:55:33 -0400

    trying path resolve

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const http = require('http').Server(app);
 
 const sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('./data/db.sqlite');
+var db = new sqlite3.Database(path.resolve('./data/db.sqlite'));
 
 db.serialize(function() {
   console.log('creating databases if they don\'t exist');
